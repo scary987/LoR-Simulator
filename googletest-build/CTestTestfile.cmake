bb12b4dd16d33c6dc9a9cfccf74c57e82f269367
Scary987 <tobyas987@googlemail.com>
2020-07-08 20:31:53 +0200

    initial commit

--- googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/scary__/LoR Simulator/googletest-src
-# Build directory: /home/scary__/LoR Simulator/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
